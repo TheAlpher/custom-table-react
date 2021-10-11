51019ccc809b30c93464c3ba96ec7644d9ccf3f2
TheAlpher <TheAlpher>
2021-10-11 22:11:54 +0530

    v1.0.7- Update Cell base styling & github link

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -8,7 +8,7 @@ props.onChange(e.target.value,props.rowIndex,props.columnIndex)
 }
 
     return (
-                <td key={props.columnIndex} className={props.editable?"editable_input_cell":"input_cell"}> 
+                <td key={props.columnIndex} className={props.editable?"editable_input_cell input_cell":"input_cell"}> 
                 {props.editable? <input onChange={onChange} className="editable_input" defaultValue={props.data}/>:props.data}
                 </td>
     )
